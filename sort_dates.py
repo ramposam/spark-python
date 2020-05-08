8cea95c72c39d6e92dfcf829cf9ac0d41896b58d
Ram Posam <65006964+ramposam@users.noreply.github.com>
2020-05-08 13:52:21 +0530

    Update sort_dates.py

--- sort_dates.py
+++ sort_dates.py
@@ -1,5 +1,5 @@
 from datetime import datetime
 dates = ['01 Apr 2017', '01 Apr 2018', '01 Aug 2017', '01 Aug 2018', '01 Dec 2017', '01 Dec 2018', '01 Feb 2017', '01 Feb 2018', '01 Jan 2017', '01 Jan 2018']
 dates.sort(key=lambda date:datetime.strptime(date,'%d %b %Y'))
-dates
+print(dates)
 
