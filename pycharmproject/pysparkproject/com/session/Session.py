0b776dbef3d289b070194edb07d1c083dcf6a7fa
ramposam <ramposam88@gmail.com>
2020-07-10 02:45:35 -0400

    remove pycharm project folder

--- pycharmproject/pysparkproject/com/session/Session.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-
-
-class Session:
-    def __init__(self):
-        self.spark = SparkSession.builder.appName("Spark Session").master("local[*]").getOrCreate()
-
-    def getSparkSession(self):
-        return self.spark
